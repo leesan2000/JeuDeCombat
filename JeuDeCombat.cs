f81db599ef0fea3d5478bddedbbf0b09069e5eb8
leesan2000 <lisandrovicens1@gmail.com>
2023-10-16 14:52:42 +0200

    class damager

--- JeuDeCombat.cs
+++ JeuDeCombat.cs
@@ -4,14 +4,49 @@ using System.Collections.Generic;
 
 
 
+
+
+class Personaje{
+    public int type;
+    public int pv;
+    public int force;
+    
+    public Personaje(int type, int pv, int force)
+    {
+        type = type;
+        pv = pv;
+        force = force;
+    }
+}
+
+class Damager : Personaje
+{
+    public int specialAttack { get; set; }
+    private int degatsEnRetour { get; set; }    
+
+    public Damager(int type, int pv, int force, int special) : base(type, pv, force)
+    {
+        specialAttack = special;
+        degatsEnRetour = 0;
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
 class Program
 {
-
-    int choix;
     static void Main()
     {
 
-        // aeaea
+
         Console.WriteLine("Best Combat Game Ever Made");
         Console.WriteLine("Bienvenue");
         Console.WriteLine("Veuillez choisir le personnage:");
@@ -19,43 +54,17 @@ class Program
         Console.WriteLine("2 - Tank");
         Console.WriteLine("3 - Damager");
 
-        choix = int.Parse(Console.ReadLine());
+        int choix = int.Parse(Console.ReadLine());
 
-
-
-
-
-
+        if(choix == 1){
+            Damager dam = new Damager(1, 3, 2, 0); 
+        }
     }
 }
 
 
 
 
-public class Damager{
-
-    int vie = 2;
-    int att = 3;
-
-    int special;
-
-
-    public void DamagerA()
-    {
 
 
 
-
-
-
-
-    }
-
-
-
-
-
-
-
-
-}

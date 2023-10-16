4936a084b67b0bd88604d88159e4e02d30886d2a
leesan2000 <lisandrovicens1@gmail.com>
2023-10-16 13:59:36 +0200

    menu

--- JeuDeCombat.cs
+++ JeuDeCombat.cs
@@ -1,1 +1,61 @@
-Console.WriteLine('Hello World    g test');
+using System;
+using System.Collections.Generic;
+
+
+
+
+class Program
+{
+
+    int choix;
+    static void Main()
+    {
+
+
+        Console.WriteLine("Best Combat Game Ever Made");
+        Console.WriteLine("Bienvenue");
+        Console.WriteLine("Veuillez choisir le personnage:");
+        Console.WriteLine("1 -  Healer");
+        Console.WriteLine("2 - Tank");
+        Console.WriteLine("3 - Damager");
+
+        choix = int.Parse(Console.ReadLine())''
+
+
+
+
+
+
+    }
+}
+
+
+
+
+public class Damager{
+
+    int vie = 2;
+    int att = 3;
+
+    int special;
+
+
+    public void DamagerA()
+    {
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
